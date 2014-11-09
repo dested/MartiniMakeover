930ef90aa08a10bcbf24d5e0d52abc3a24246dbd
Zak <Zak@blueglobalmedia.com>
2014-11-09 15:26:24 -0700

    first commit. All of bootstrap and the starts of a menu.

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta content="IE=edge" http-equiv="X-UA-Compatible">
+		<meta content="width=device-width, initial-scale=1" name="viewport">
+		<meta content="" name="description">
+		<meta content="" name="author">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link href="../favicon.ico" rel="icon">
+		<title>MartiniMakeover For all Your Makeup Package Needs</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid" style="width: 50%; center">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">MartiniMakeover</a>
+				</div>
+				<div style="float: right;">
+					<ul class="nav navbar-nav">
+						<li><a href="index.php">HOME</a></li>
+						<li><a href="about.php">ABOUT MARTINIMAKEOVER</a></li>
+						<li><a href="contact.php">CONTACT US</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>		
+
+<?php
